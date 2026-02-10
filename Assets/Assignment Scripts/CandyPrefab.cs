64de264158641dac8aa1a4ccfd03330df364307f
ferremia <ferremia@OA-S141-12>
2026-02-10 09:37:12 -0500

    drop working

--- Assets/Assignment Scripts/CandyPrefab.cs
+++ Assets/Assignment Scripts/CandyPrefab.cs
@@ -6,26 +6,36 @@ public class CandyPrefab : MonoBehaviour
     public float speed = 0.1f;
     public float rotationSpeed = 0.1f;
     public Vector3 position;
+    public float dist;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //get object position
-        position = transform.position;
-
-
         
-
-
-        //add speed to transform
-        position.y += speed;
-
-        //correct the object position
-        transform.position = position;
     }
 
     // Update is called once per frame
     void Update()
+    {
+        //get object position
+        position = transform.position;
+
+        //add speed to transform
+        position.y -= speed;
+
+        //do rotation
+        position.z += rotationSpeed;
+
+        //correct the object position
+        transform.position = position;
+
+        if (position.y < -6)
+        {
+            Object.Destroy(gameObject);
+        }
+    }
+
+    public void CollisionCheck ()
     {
         
     }
